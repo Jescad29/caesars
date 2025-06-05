cc6a85de1dd10d28268bc53c2603f535924dbfc1
Chuy <jescad27@gmail.com>
2025-06-05 12:41:38 -0600

    Creacion de funcion encript

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'
             ]
 
-direction =input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text =input("Type your message:\n").lower()
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+
+def encript(original_text, shift_amount):
+    cipher_text = ""
+
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        cipher_text += alphabet[shifted_position]
+
+    print(f"Here is the encoded result: {cipher_text}")
